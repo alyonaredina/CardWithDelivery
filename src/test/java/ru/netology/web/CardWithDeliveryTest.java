422a8631a5a9cf38a9f2bbff104a04615de6ba49
Alyona Redina <medvedeva_64@mail.ru>
2024-06-25 01:36:22 +0600

    add invalid project

---
+++ src/test/java/ru/netology/web/CardWithDeliveryTest.java
@@ -0,0 +1,39 @@
+package ru.netology.web;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardWithDeliveryTest {
+
+    String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        //SelenideElement form = $(".form");
+        $("[data-test-id=city] input").setValue("Казань");
+        String planningDate = generateDate(4, "dd.mm.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Иван Петров");
+        $("[data-test-id=phone] input").setValue("+79000000000");
+        $("[data-test-id=agreement]").click();
+        $(".button__button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Встреча успешно забронирована на 28.06.2024" + planningDate));
+    }
+
+   
+}
+
+
